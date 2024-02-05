2993640b41fafdc7698057e86ac266663a9c76ee
SnJon <snjon93@gmail.com>
2024-02-05 16:40:49 +0900

    add neopixel (test)

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,16 +5,35 @@
 
 TLV *packet = modbus_tlv();
 
+unsigned long patternPrevious = 0; // test neopixel
+byte patternCurrent = 0;
+
 void setup()
 {
   Serial.begin(115200);
+  Serial3.begin(115200);
   servo_setup();
+
+  // servo_power(false); // test neopixel
+  // patternCurrent = 1;
 }
 
 void loop()
 {
   servo_tick();
   // ultrasonic_ping();
+
+  if (millis() - patternPrevious >= 10000)
+  {
+    patternPrevious = millis();
+    Serial3.write(patternCurrent);
+    patternCurrent++;
+
+    if (patternCurrent > 5)
+    {
+      patternCurrent = 1;
+    }
+  }
 }
 
 void serialEvent()
